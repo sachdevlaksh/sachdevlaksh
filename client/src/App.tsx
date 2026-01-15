909ec216afabdfb6a99a5d1cf25257ca73bef6ca
Lakshay Sachdeva <35967999+sachdevlaksh@users.noreply.github.com>
2026-01-15 19:32:47 +0530

    Configure wouter router with BASE_URL for GitHub Pages subdirectory

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,7 +6,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
 
-function Router() {
+function AppRouter() {
   return (
     <Switch>
       <Route path="/" component={Home} />
@@ -19,8 +19,10 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Toaster />
-        <Router />
+        <Router base={import.meta.env.BASE_URL}>
+          <Toaster />
+          <AppRouter />
+        </Router>
       </TooltipProvider>
     </QueryClientProvider>
   );
